68f4153412535b4909c7272d166cd11672111cf3
KCaverly <kylebcaverly@gmail.com>
2023-12-13 12:04:24 -0500

    removed function for predict.py

--- phind_codellama_34b_v2_gguf/predict.py
+++ phind_codellama_34b_v2_gguf/predict.py
@@ -17,7 +17,6 @@ class Predictor(BasePredictor):
         system_prompt: str = Input(description="System prompt for the model, helps guides model behaviour.", default=SYSTEM_PROMPT),
         prompt_template: str = Input(description="Template to pass to model. Override if you are providing multi-turn instructions.", default=PROMPT_TEMPLATE),
         max_new_tokens: int = Input(description='Maximum new tokens to generate.', default=-1),
-        do_sample: bool = Input(description="if set to True, this parameter enables decoding strategies such as multinomial sampling, beam-search multinomial sampling, Top-K sampling and Top-p sampling. All these strategies select the next token from the probability distribution over the entire vocabulary with various strategy-specific adjustments.", default=True),
         top_p: float = Input(description="This parameter controls how many of the highest-probability words are selected to be included in the generated text", default=0.75),
         top_k: int = Input(description="This is the number of probable next words, to create a pool of words to choose from", default=40),
         temperature: float = Input(description="This parameter used to control the 'warmth' or responsiveness of an AI model based on the LLaMA architecture. It adjusts how likely the model is to generate new, unexpected information versus sticking closely to what it has been trained on. A higher value for this parameter can lead to more creative and diverse responses, while a lower value results in safer, more conservative answers that are closer to those found in its training data. This parameter is particularly useful when fine-tuning models for specific tasks where you want to balance between generating novel insights and maintaining accuracy and coherence.", default=0.01),
@@ -26,5 +25,5 @@ class Predictor(BasePredictor):
 
         full_prompt = prompt_template.replace("{prompt}", prompt).replace("{system_prompt}", system_prompt)
 
-        for output in self.model(full_prompt, stream=True, do_sample=do_sample, top_p=top_p, top_k=top_k, max_tokens=max_new_tokens, temperature=temperature):
+        for output in self.model(full_prompt, stream=True, top_p=top_p, top_k=top_k, max_tokens=max_new_tokens, temperature=temperature):
             yield output['choices'][0]['text']
