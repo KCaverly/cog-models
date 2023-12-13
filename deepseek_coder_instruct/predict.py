a7682193c8ba070d603ec4839afd9b3c81c8df54
KCaverly <kylebcaverly@gmail.com>
2023-12-13 18:06:35 -0500

    formatted

--- deepseek_coder_instruct/predict.py
+++ deepseek_coder_instruct/predict.py
@@ -6,6 +6,7 @@ from cog import BasePredictor, Input, ConcatenateIterator
 from transformers import AutoTokenizer, AutoModelForCausalLM, TextIteratorStreamer
 from typing import List
 
+
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
@@ -16,26 +17,43 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        messages: str = Input(description='Chat messages, passed as a json string'),
-        max_new_tokens: int = Input(description='Maximum new tokens to generate.', default=512),
-        do_sample: bool = Input(description="Whether or not to use sampling; use greedy decoding otherwise.", default=False),
-        top_k: int = Input(description="The number of highest probability vocabulary tokens to keep for top-k filtering.", default=None),
-        top_p: float = Input(description="If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.", 
-                             default=None),
-        num_return_sequences: int = Input(description="The number of independently computed returned sequences for each element in the batch.", default=1),
+        messages: str = Input(description="Chat messages, passed as a json string"),
+        max_new_tokens: int = Input(
+            description="Maximum new tokens to generate.", default=512
+        ),
+        do_sample: bool = Input(
+            description="Whether or not to use sampling; use greedy decoding otherwise.",
+            default=False,
+        ),
+        top_k: int = Input(
+            description="The number of highest probability vocabulary tokens to keep for top-k filtering.",
+            default=None,
+        ),
+        top_p: float = Input(
+            description="If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.",
+            default=None,
+        ),
+        num_return_sequences: int = Input(
+            description="The number of independently computed returned sequences for each element in the batch.",
+            default=1,
+        ),
     ) -> ConcatenateIterator[str]:
         """Run a single prediction on the model"""
 
         input_strings = json.loads(messages)
 
-        inputs = self.tokenizer.apply_chat_template(input_strings, return_tensors='pt').to(self.model.device)
-        _ = self.model.generate(inputs, 
-                                streamer=self.streamer, 
-                                max_new_tokens=max_new_tokens, 
-                                do_sample=do_sample, 
-                                top_k=top_k, 
-                                top_p=top_p, 
-                                num_return_sequences=num_return_sequences)
+        inputs = self.tokenizer.apply_chat_template(
+            input_strings, return_tensors="pt"
+        ).to(self.model.device)
+        _ = self.model.generate(
+            inputs,
+            streamer=self.streamer,
+            max_new_tokens=max_new_tokens,
+            do_sample=do_sample,
+            top_k=top_k,
+            top_p=top_p,
+            num_return_sequences=num_return_sequences,
+        )
 
         i = 0
         for word in self.streamer:
--- nexus_raven_v2_gguf/predict.py
+++ nexus_raven_v2_gguf/predict.py
@@ -1,17 +1,31 @@
 from cog import BasePredictor, Input, ConcatenateIterator
 from llama_cpp import Llama
 
+
 class Predictor(BasePredictor):
     def setup(self) -> None:
-        self.model = Llama(model_path="./nexusraven-v2-13b.Q5_K_M.gguf", n_gpu_layers=-1, n_ctx=8192, n_threads=1, main_gpu=0)
+        self.model = Llama(
+            model_path="./nexusraven-v2-13b.Q5_K_M.gguf",
+            n_gpu_layers=-1,
+            n_ctx=8192,
+            n_threads=1,
+            main_gpu=0,
+        )
 
     def predict(
         self,
         prompt: str = Input(description="Instruction for model"),
-        max_new_tokens: int = Input(description="Maximum new tokens to generate.", default=-1),
-        temperature: float = Input(description="This parameter used to control the 'warmth' or responsiveness of an AI model based on the LLaMA architecture. It adjusts how likely the model is to generate new, unexpected information versus sticking closely to what it has been trained on. A higher value for this parameter can lead to more creative and diverse responses, while a lower value results in safer, more conservative answers that are closer to those found in its training data. This parameter is particularly useful when fine-tuning models for specific tasks where you want to balance between generating novel insights and maintaining accuracy and coherence.", default=0.001),
-        ) -> ConcatenateIterator[str]:
+        max_new_tokens: int = Input(
+            description="Maximum new tokens to generate.", default=-1
+        ),
+        temperature: float = Input(
+            description="This parameter used to control the 'warmth' or responsiveness of an AI model based on the LLaMA architecture. It adjusts how likely the model is to generate new, unexpected information versus sticking closely to what it has been trained on. A higher value for this parameter can lead to more creative and diverse responses, while a lower value results in safer, more conservative answers that are closer to those found in its training data. This parameter is particularly useful when fine-tuning models for specific tasks where you want to balance between generating novel insights and maintaining accuracy and coherence.",
+            default=0.001,
+        ),
+    ) -> ConcatenateIterator[str]:
         """Run a single prediction on the model"""
 
-        for output in self.model(prompt, stream=True, temperature=temperature, max_tokens=max_new_tokens):
-            yield output['choices'][0]['text']
+        for output in self.model(
+            prompt, stream=True, temperature=temperature, max_tokens=max_new_tokens
+        ):
+            yield output["choices"][0]["text"]
