bd21af45c1a907867c8270dc7920a6b3ce3b630a
KCaverly <kylebcaverly@gmail.com>
2024-01-10 19:28:52 -0500

    fixed model name for mega dolphin

--- mega_dolphin_120b_gguf/download.sh
+++ mega_dolphin_120b_gguf/download.sh
@@ -1,1 +1,1 @@
-huggingface-cli download TheBloke/megadolphin-120b-GGUF megadolphin-120b.Q4_K_M.gguf --local-dir . --local-dir-use-symlinks False
+huggingface-cli download TheBloke/MegaDolphin-120b-GGUF megadolphin-120b.Q4_K_M.gguf --local-dir . --local-dir-use-symlinks False
